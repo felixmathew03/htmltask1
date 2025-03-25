64023fd888e73792b4c58bdd92626438a9e81763
felixmathew03 <felixmathewtt@gmail.com>
2025-03-25 12:10:44 +0530

    R18

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,9 @@
 function displayNav() {
     document.getElementById('threedots').style.display = 'none';
-    document.getElementById('header').style.background='none';
     document.getElementById('twolines').style.display = "flex";
 }
 
 function removeNav() {
     document.getElementById('threedots').style.display = 'block';
-    document.getElementById('header').style.backgroundImage="url('./assets/images/fbg.png')";
-    document.getElementById('header').style.backgroundPosition='center';
-    document.getElementById('header').style.backgroundSize='cover';
     document.getElementById('twolines').style.display = "none";
-    document.getElementById('navSection').classList.remove("show"); 
-    document.getElementById('navfoot').classList.remove("show"); 
-}
-
-function displayServices(){
-    document.getElementById('navSection').classList.add("show"); 
-    document.getElementById('navfoot').classList.add("show"); 
 }
